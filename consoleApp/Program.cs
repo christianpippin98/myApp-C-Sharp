0d789750f0d0553d1de5f2a1eb39ed1f45112326
Christian Pippin <ianpippin@gmail.com>
2020-08-06 13:57:05 -0500

    sixth commit

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -3,17 +3,22 @@
 namespace Intro {
     public class Program {
         public static void Main (string[] args) {
-            string secretNumber = ("65");
+            Random rnd = new Random ();
+            int secretNumber = rnd.Next (1, 101);
             for (int i = 1; i < 5; i++) {
                 Console.Write ("Guess a number from 1-100 ");
-                string number = Console.ReadLine ();
+                int number = Convert.ToInt32 (Console.ReadLine ());
                 int guesses = (4 - i);
 
-                if (number == secretNumber) {
+                if (0 == guesses) {
+                    Console.Write ("Sorry, that was not right! You have no guesses left!");
+                } else if (number == secretNumber) {
                     Console.Write ("You Guessed Correctly!");
                     break;
-                } else {
-                    Console.Write ($"Sorry! That is not correct. You have {guesses} guess(es) remaining! ");
+                } else if (number > secretNumber) {
+                    Console.Write ($"Sorry! That is not correct. Guess lower! You have {guesses} guess(es) remaining! ");
+                } else if (number < secretNumber) {
+                    Console.Write ($"Sorry! That is not correct. Guess higher! You have {guesses} guess(es) remaining! ");
                 }
             }
         }
